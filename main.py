5508a50282268d3cc99ef52420250e0420b00746
OumaymaDourrou <salamoumayma@gmail.com>
2023-04-05 12:27:31 +0200

    MODIFICATION

---
+++ main.py
@@ -0,0 +1,8 @@
+def somma_due_numeri(a:float,b:float):
+    '''questa funziona somma due numeri float e restituisce il 
+    risultatosommata'''
+    somma=a+b
+    print(somma)
+    return somma
+
+somma_due_numeri(5,6)
